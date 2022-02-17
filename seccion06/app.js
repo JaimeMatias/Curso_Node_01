647656d6602e593d0f01dce8ac9ec868ba7f6f04
JaimeMatias <matias.raul.jaime@gmail.com>
2022-02-17 08:21:28 -0300

    Contenido Seccion 6 Controlador de la Aplicación

--- seccion06/app.js
+++ seccion06/app.js
@@ -1,24 +1,35 @@
-const {inquierer_menu,pausa} = require('../seccion06/helper/inquirer');
+const {inquierer_menu,pausa,leer_imput} = require('../seccion06/helper/inquirer');
+const {Busquedas}=require('../seccion06/models/busquedas');
+require('colors');
 const main=async() => {
     
     let opt=1;
     let esperar=true;
+    let busqueda=new Busquedas();
     do {
         opt= await inquierer_menu();
         switch (opt) {
             case 1:
-                console.log('Selecionó la opción 1')
-                esperar=true    
+                console.log('Selecionó la opción 1');
+                const ciudad_buscada= await leer_imput('Por favor ingrese una ciudad');
+                busqueda.ciudad(ciudad_buscada);
+                console.log(`Información de la ciudad:`.green);
+                console.log(`Ciudad: `);
+                console.log(`Lat:`);
+                console.log(`Lng: `);
+                console.log(`Temperatura:`);
+                console.log(`Mínima:`);
+                console.log(`Maxima:`);
+                esperar=true;    
                 break;
             case 2:
-                console.log('Selecionó la opción 2')    
-                esperar=true
+                console.log('Selecionó la opción 2')    ;
+                esperar=true;
                 break;
             case 0:
-                console.log('Selecionó salir')    
-                esperar=false
+                console.log('Selecionó salir')    ;
+                esperar=false;
         }
-        console.log(opt);
     if(esperar)await pausa();
 
     } while (opt!==0);
