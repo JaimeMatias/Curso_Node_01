103f1a07959519f9e5fed69921b8ecd9dfdf64dc
JaimeMatias <matias.raul.jaime@gmail.com>
2022-02-23 08:53:48 -0300

    Contenido Seccion 9 Modelo de Usuario

---
+++ seccion09/models/usuario.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        require: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        require: [true, 'El correo es obligatorio'],
+        unique:true,
+    },
+    Password:{
+        type: String,
+        require: [true, 'La contraseña es obligatorio'],  
+    },
+    img:{
+        type:String
+    },
+    rol:{
+        type:String,
+        required:true,
+        emun:['ADMIN_ROLE','USER_ROLE' ],
+    },
+    estado:{
+        type:Boolean,
+default:true,
+        
+    },
+    google:{
+        type:Boolean,
+        default:false
+    }
+
+});
+
+module.exports = model('Usuario',UsuarioSchema);
