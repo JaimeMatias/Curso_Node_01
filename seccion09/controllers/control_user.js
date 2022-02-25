40f20a7ec3ebb8c6341c3432e8894c72829843fd
JaimeMatias <matias.raul.jaime@gmail.com>
2022-02-24 21:57:12 -0300

    Contenido Seccion 9 Metodo Get

--- seccion09/controllers/control_user.js
+++ seccion09/controllers/control_user.js
@@ -1,56 +1,68 @@
 const { request, response } = require('express');
-const bcryptjs =require('bcryptjs');
-const {validationResult}=require('express-validator');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 const Usuario = require('../models/usuario');
-const {validar_campos}=require('../middlewares/validar_campos')
+const { validar_campos } = require('../middlewares/validar_campos')
 
+//----------------GET-----------------------------
+const usuarioGet = async (req = request, res = response) => {
+   const { limite, desde } = req.query;
 
-const usuarioGet = (req, res = response) => {
-    
-    const { nombre } = req.query;
+    //el Promise.all ejecuta un arreglo de promesas todas en simultaneo
+    //const resp = await Promise.all ([
+    const [total,usuarios] = await Promise.all ([
+        Usuario.countDocuments({ estado: true }),
+        Usuario.find({ estado: true })
+            .limit(limite)
+            .skip(desde)
+
+    ]);
+
     res.status(200).json({
-        nombre,
-        'msg': "Get API - Controlador",
+        total,
+        usuarios
+
     });
 }
 
-
+//----------------POST-----------------------------
 const usuarioPost = async (req = request, res = response) => {
-   
-    const {nombre,correo,edad,Password,rol} = req.body;
-    const salt=bcryptjs.genSaltSync();
-    
+
+    const { nombre, correo, edad, Password, rol } = req.body;
+    const salt = bcryptjs.genSaltSync();
+
     //Verificar si el correo existe
 
-    
+
     //Encriptar la contraseña:
-    const usuario = new Usuario({nombre,correo,edad,Password,rol});
-    usuario.Password=bcryptjs.hashSync(Password,salt);
+    const usuario = new Usuario({ nombre, correo, edad, Password, rol });
+    usuario.Password = bcryptjs.hashSync(Password, salt);
     await usuario.save();
 
     console.log(usuario);
     res.status(200).json({
         'msg': "Post API - ",
         usuario,
-        
+
     });
 }
 
-const usuariosPut = async(req, res = response) => {
+//----------------POST-----------------------------
+const usuariosPut = async (req, res = response) => {
     const id = req.params.id_usuario;
-    const {_id,Password,google,correo,...resto}=req.body;
+    const { _id, Password, google, correo, ...resto } = req.body;
 
-    if (Password){
-        const salt=bcryptjs.genSaltSync();
-        resto.Password=bcryptjs.hashSync(Password,salt);
+    if (Password) {
+        const salt = bcryptjs.genSaltSync();
+        resto.Password = bcryptjs.hashSync(Password, salt);
     }
-    const usuario =await Usuario.findByIdAndUpdate(id,resto);
-    res.status(200).json({
-        'msg': "Put API - Controlador",
-        usuario,
-    });
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+    res.status(200).json(usuario);
 }
+
+
+
 const usuarioPatch = (req, res = response) => {
     res.status(200).json({
         'msg': "Patch API - Controlador",
