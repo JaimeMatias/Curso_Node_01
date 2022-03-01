aba629f2620e88d396a0e956c2a8ee2272342ec4
JaimeMatias <matias.raul.jaime@gmail.com>
2022-03-01 10:33:43 -0300

    Contenido Seccion 11 Despliegue Heroku

--- seccion11/controllers/control_auth.js
+++ seccion11/controllers/control_auth.js
@@ -1,75 +1,104 @@
 const { request, response } = require('express');
 const bcryptjs = require('bcryptjs');
 
-const Usuario =require('../models/usuario');
-const {generarjwt}=require('../helper/generar_jwt');
-const {google_verify}=require('../helper/google_verify');
-const login =async(req=request,res=response)=>{
-    const {correo,Password}=req.body;
+const Usuario = require('../models/usuario');
+const { generarjwt } = require('../helper/generar_jwt');
+const { google_verify } = require('../helper/google_verify');
+const login = async (req = request, res = response) => {
+    const { correo, Password } = req.body;
     try {
 
 
-        const usuario= await Usuario.findOne({correo});
-       
-        if(!usuario){
+        const usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
             return res.status(400).json({
-                msg:'Usuario / Password no son correctos -correo'
+                msg: 'Usuario / Password no son correctos -correo'
             })
-        }else{
-            
-            if(!usuario.estado){
+        } else {
+
+            if (!usuario.estado) {
                 return res.status(400).json({
-                    msg:'Usuario dado de baja de la base de datos'
+                    msg: 'Usuario dado de baja de la base de datos'
                 })
             };
 
-            const valid_password= bcryptjs.compareSync(Password,usuario.Password);
-            
-            if(!valid_password){
+            const valid_password = bcryptjs.compareSync(Password, usuario.Password);
+
+            if (!valid_password) {
                 return res.status(400).json({
-                    msg:'Contraseña Incorrecta',
+                    msg: 'Contraseña Incorrecta',
                     valid_password
                 })
             }
-            const token=await generarjwt(usuario.id);
+            const token = await generarjwt(usuario.id);
             res.json({
-                msg:'Login Exitoso!!!!',
+                msg: 'Login Exitoso!!!!',
                 token
-              
+
             })
         }
-        
 
-        
+
+
 
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            msg:'Hable con el administrador'
+            msg: 'Hable con el administrador'
         })
-        
+
     }
 
 }
 
-const googleSignIn =async (req=request,res=response)=>{
+const googleSignIn = async (req = request, res = response) => {
     //console.log(req)
-const {id_token}=req.body;
-try {
-    const {nombre,img,correo}=await google_verify(id_token);
-} catch (error) {
-    res.status(500).json({
-        msg:'Error',
-        error
-    })
-}
-res.json({
-    msg:'Todo bien',
-    id_token
-})
+    const { id_token } = req.body;
+    try {
+        const { nombre, img, correo } = await google_verify(id_token);
+        const usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                img,
+                Password: 'asd',
+                google: true,
+                rol: 'USER_ROLE'
+
+            }
+            console.log('Creando Usuario');
+            const nuevo_usuario = new Usuario(data);
+            await nuevo_usuario.save();
+            console.log('Usuario Creado Base de Datos')
+        } else {
+            if (!usuario.estado) {
+                return res.status(401).json({
+                    msg: 'Hable con el administrador, usuario bloqueado'
+                })
+            }
+
+            const token = await generarjwt(usuario.id);
+            res.json({
+                usuario,
+                token
+
+            })
+
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Problema para validar token',
+            error
+        })
+    }
+
 }
 
-module.exports ={
+module.exports = {
     login,
     googleSignIn
 }
