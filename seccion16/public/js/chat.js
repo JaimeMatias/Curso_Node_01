6de714a6f89eb18ec789a7322abce360725a0622
JaimeMatias <matias.raul.jaime@gmail.com>
2022-03-24 21:05:19 -0300

    Contenido Seccion 16 HTML y JS

--- seccion16/public/js/chat.js
+++ seccion16/public/js/chat.js
@@ -1,40 +1,66 @@
 //const { validarJWT } = require("../../middlewares");
 
-let usuario=null;
-let socket=null;
+let usuario = null;
+let socket = null;
+
+const txtUid = document.querySelector('#txtUid')
+const txtMensaje = document.querySelector('#txtMensaje')
+const ulUsuarios = document.querySelector('#ulUsuarios')
+const ulMensajes = document.querySelector('#ulMensajes')
+const btnSalir = document.querySelector('#btnSalir')
 var url = (window.location.hostname.includes('localhost'))
     ? 'http://localhost:8080/api/auth/'
     : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
 
-const validarJWT =async()=>{
-    const token=localStorage.getItem('token')||'';
-    if (token.length<=10){
-        window.location='index.html';
+const validarJWT = async () => {
+    const token = localStorage.getItem('token') || '';
+    if (token.length <= 10) {
+        window.location = 'index.html';
         throw new Error('No hay token en el servidor');
     }
-    const resp= await fetch(url,{
-        headers:{'x-token':token}
+    const resp = await fetch(url, {
+        headers: { 'x-token': token }
     });
 
-    const {usuario:userDB,token:TokenDB}=await resp.json()
-    console.log(userDB,TokenDB)
-    localStorage.setItem('token',TokenDB);
-    usuario=userDB;
-    document.title=usuario.nombre;
+    const { usuario: userDB, token: TokenDB } = await resp.json()
+    console.log(userDB, TokenDB)
+    localStorage.setItem('token', TokenDB);
+    usuario = userDB;
+    document.title = usuario.nombre;
 
     await conectarSocket();
 }
 
-const conectarSocket =async()=>{
-    const socket =io({
-        'extraHeaders':{
-            'x-token':localStorage.getItem('token')
+const conectarSocket = async () => {
+    socket = io({
+        'extraHeaders': {
+            'x-token': localStorage.getItem('token')
         }
+    });
+
+    socket.on('connect',()=>{
+        console.log('Sockets Online')
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('Sockets Offline')
+    });
+
+    socket.on('recibir-mensajes',()=>{
+
+    });
+    
+    socket.on('usuarios-activos',()=>{
+
+    });
+    
+    socket.on('mensaje-privado',()=>{
+
     });
 }
 
-const main=async()=>{
-await validarJWT();
+const main = async () => {
+    await validarJWT();
 }
 main()
 //const socket=io()
